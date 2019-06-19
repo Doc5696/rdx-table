f81a86b9c7d1cd3aba5b57fff1d6d21476f257d5
Doc5696 <root.itbox@gmail.com>
2019-06-19 09:36:12 +0300

    initial commit

---
+++ src/containers/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header';
+import Table from '../Table';
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <Header />
+        <Table />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Table/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Table extends React.Component {
+  render() {
+    return(
+      <table>
+        Table
+      </table>
+    );
+  }
+}
+
+export default Table;
